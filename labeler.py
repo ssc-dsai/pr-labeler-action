aea8321c3e525c780cffc94e7c4e568780c079d5
Pascal Beaulne <pascalbeaulne@Pascals-MacBook-Pro.local>
2022-01-31 11:54:21 -0500

    Fixed labeler issue

--- labeler.py
+++ labeler.py
@@ -1,6 +1,4 @@
-from tkinter import Label
-from turtle import color
-from github import Github, Label
+from github import Github
 import os
 
 
@@ -12,6 +10,13 @@ COMMAND         = os.getenv('COMMAND')
 LABEL_NAME      = os.getenv('LABEL_NAME')
 LABEL_COLOUR    = os.getenv('LABEL_COLOUR')
 
+def find_label(labels):
+    foundLabel = False
+    for label in labels:
+         if label.name == LABEL_NAME:
+             foundLabel = True
+    return foundLabel
+
 def main():
     github = Github(GITHUB_TOKEN)
     repo = github.get_repo(REPO_NAME)
@@ -19,9 +24,21 @@ def main():
 
     # Verify COMMAND
     if COMMAND == "ADD":
-        label = Label.Label(name=LABEL_NAME, color=LABEL_COLOUR)
-        pull_request.set_labels(label)
+        # Check if the Label already exist
+        labels = repo.get_labels()
+        label_exist = find_label(labels)
+
+        if label_exist:
+            # Set label to PR
+            pull_request.set_labels(LABEL_NAME)
+        else:
+            # Create Label to use
+            label = repo.create_label(name=LABEL_NAME, color=LABEL_COLOUR, description="Github Action Label")
+            # Set label to PR
+            pull_request.set_labels(label)
+
     elif COMMAND == "REMOVE":
+        # Removing label from PR
         pull_request.delete_labels()
 
 if __name__ == "__main__":
